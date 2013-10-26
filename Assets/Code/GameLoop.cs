dcda6950233fb420fc9023e6c442ca6056983ef4
Justin <justin@lordpi.com>
2013-10-26 15:49:58 -0700

    Start of board spawning

--- Assets/Code/GameLoop.cs
+++ Assets/Code/GameLoop.cs
@@ -1,14 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 using BaconGameJam6.Models.Simulations;
+using BaconGameJam6.Models.Boards;
 using BaconGameJam6.Models.Blocks;
 
 public class GameLoop : MonoBehaviour {
 	
+	public GameObject Board;
+	public GameObject BlueBlock;
+	public GameObject Ship;
+
 	private Simulation simulation;
 	
 	void Start () {
 		this.simulation = new Simulation();
+		
+		Board board = this.simulation.Board;
+		GameObject boardView = Instantiate(Board, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+		
+		foreach (Block block in board)
+		{
+			GameObject blockView = Instantiate(BlueBlock, new Vector3(block.X, block.Y, block.Z), Quaternion.identity) as GameObject;
+			blockView.transform.parent = boardView.transform;
+		}
+		
+		// $TODO: get ship
+		GameObject shipView = Instantiate(Ship, new Vector3(3, 9, 0), Quaternion.identity) as GameObject;
+		shipView.transform.parent = boardView.transform;
 	}
 	
 	void Update () {
