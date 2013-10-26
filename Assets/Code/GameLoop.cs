a7a86520a727919b3601d6c04ec1c6d23eb53cc1
Alex Schearer <aschearer@gmail.com>
2013-10-26 15:02:35 -0700

    Adding game loop script

---
+++ Assets/Code/GameLoop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using BaconGameJam6.Models.Simulations;
+using BaconGameJam6.Models.Blocks;
+
+public class GameLoop : MonoBehaviour {
+	
+	private Simulation simulation;
+	
+	void Start () {
+		this.simulation = new Simulation();
+	}
+	
+	void Update () {
+	}
+	
+}
