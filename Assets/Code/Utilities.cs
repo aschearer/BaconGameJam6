fc332b682f8c6a341bde2e0c3ecc1c22d6ffc5b4
Alex Schearer <aschearer@gmail.com>
2013-10-27 11:49:36 -0700

    Updating color values

--- Assets/Code/Utilities.cs
+++ Assets/Code/Utilities.cs
@@ -12,17 +12,17 @@ namespace BaconGameJam6
                 case BlockType.Black:
                     return Color.black;
                 case BlockType.Blue:
-                    return Color.blue;
+                    return new Color(0, 255f / 255f, 231f / 255f);
                 case BlockType.Green:
-                    return Color.green;
+                    return new Color(0, 218f / 255f, 60f / 255f);
                 case BlockType.Purple:
-                    return new Color(1f, 0f, 1f, 1f);
+                    return new Color(255, 0f, 255f / 255f);
                 case BlockType.Red:
-                    return Color.red;
+                    return new Color(223f / 255f, 21f / 255f, 26f / 255f);
                 case BlockType.White:
                     return Color.white;
                 case BlockType.Yellow:
-                    return Color.yellow;
+                    return new Color(244f / 255f, 243f / 255f, 40f / 255f);
             }
 
             return Color.black;
