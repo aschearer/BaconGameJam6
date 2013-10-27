0e4f9fa377f93b26b6091bf7fce9bb39d6330a43
IMakeIndieGames <ty@case.edu>
2013-10-26 18:34:03 -0700

    Optimizes background

--- Assets/Code/BackgroundGridScript.cs
+++ Assets/Code/BackgroundGridScript.cs
@@ -6,9 +6,11 @@ public class BackgroundGridScript : MonoBehaviour
 {
     public GameObject cubeModel;
 
-    private const int GridWidth = 100;
-    private const int GridHeight = 60;
-    private const float InitialZPosition = -5;
+    private const int GridXStart = -40;
+    private const int GridWidth = 80;
+    private const int GridYStart = -10;
+    private const int GridHeight = 40;
+    private const float InitialZPosition = -3;
     private const float MaximumZOffset = 0.2f;
     private const float MinimumSpeed = 0.2f;
     private const float MaximumSpeed = 1f;
@@ -27,11 +29,11 @@ public class BackgroundGridScript : MonoBehaviour
         this.allCubeModels = new List<BackgroundCubeData>();
 
         // Create a grid of background cubes.
-        for (int y = -GridHeight / 2; y < GridHeight / 2; ++y)
+        for (int y = GridYStart; y < GridHeight + GridYStart; ++y)
         {
-            for (int x = -GridWidth / 2; x < GridWidth / 2; ++x)
+            for (int x = GridXStart; x < GridWidth + GridXStart; ++x)
             {
-                GameObject backgroundCube = Instantiate(this.cubeModel, new Vector3(x / 2f, y / 2f, InitialZPosition), Quaternion.identity) as GameObject;
+                GameObject backgroundCube = Instantiate(this.cubeModel, new Vector3(x * 0.5f, y * 0.5f, InitialZPosition), Quaternion.identity) as GameObject;
                 backgroundCube.renderer.material.color = Color.Lerp(Color.Lerp(Color.white, Color.black, Random.Range(0.33f, 0.66f)), Color.blue, 0.1f);
 
                 this.allCubeModels.Add(new BackgroundCubeData()
