f8a71ddbe1725d7f916f6c268e2e6e514875c402
IMakeIndieGames <ty@case.edu>
2013-10-27 12:52:06 -0700

    Stubs the UI Manager

---
+++ Assets/UIManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public GameObject Player1StartPlane;
+    public GameObject Player2StartPlane;
+    public GameObject Player3StartPlane;
+    public GameObject Player4StartPlane;
+    public GameObject InGamePlane;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
