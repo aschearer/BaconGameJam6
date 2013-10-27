2a4f5d80f65f5929abc51dd3a9ab395a599a9177
Alex Schearer <aschearer@gmail.com>
2013-10-27 04:30:41 -0700

    Removing power ups

--- Assets/Code/Models/Blocks/BlockFactory.cs
+++ Assets/Code/Models/Blocks/BlockFactory.cs
@@ -2,8 +2,6 @@
 {
     using System;
 
-    using Assets.Code.Models.Powerups;
-
     using BaconGameJam6.Models.Boards;
 
     public class BlockFactory
@@ -20,10 +18,6 @@
         public Block CreateBlock(int col, int row, int level)
         {
             var blockType = (BlockType)RandomGenerator.Next(2 + level);
-            if (blockType == BlockType.Black)
-            {
-                return new PowerUp(this.board, col, row, PowerUpType.Bomb);
-            }
 
             return new Block(this.board, col, row, blockType);
         }
